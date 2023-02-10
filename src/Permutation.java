bf4d77534724f936290e89f5b1994a5ee4659e2a
tyler <ttang7910@bths.edu>
2023-02-10 12:34:27 -0500

    only wrote loops loops are inverted

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Permutation {
     public static void main(String[] args) {
-        
+        int[] num = {1,1,1};
+
+        for(int i = 0; i < num.length; i++) {
+            for (int j = 0; j < 2; j++) {
+                for(int x = 3; x < 0; x--) {
+                    num[i]++;
+                }
+            }
+        }
     }
 }
