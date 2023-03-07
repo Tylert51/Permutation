078ff5df4addc3a2fb88af3b357fc5cc8ddef68a
tyler <ttang7910@bths.edu>
2023-03-07 12:33:45 -0500

    messed up main 2

--- src/Main2.java
+++ src/Main2.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
 
-        int[] possibleNums = {1,2,3,4,5,6, 7, 8};
+        int[] possibleNums = {1,2,3,4};
         ArrayList<ArrayList> possibleNums2 = new ArrayList<ArrayList>();
         ArrayList<ArrayList> perm = new ArrayList<ArrayList>();
 
         ArrayList<ArrayList> perm2;
+        ArrayList<Integer> newPerm = new ArrayList<>();
 
         for(int i = 0; i < possibleNums.length; i++) {
             possibleNums2.add(new ArrayList<Integer>(Arrays.asList(possibleNums[i])));
@@ -16,16 +17,17 @@ public class Main {
 
         perm2 = (ArrayList<ArrayList>) possibleNums2.clone();
 
-        for (int j = 0; j < 5; j++) {
+        for (int j = 0; j < 3; j++) {
             for (int i = 0; i < possibleNums2.size(); i++) {
 
                 for (int x = 0; x < perm2.size(); x++) {
-                    ArrayList<Integer> temp = (ArrayList<Integer>) possibleNums2.get(i).clone();
+                    ((ArrayList<Integer>) possibleNums2.get(i).clone()).removeAll(newPerm);
+                    ArrayList<Integer> temp = possibleNums2.get(i);
                     ArrayList<Integer> perm2C = (ArrayList<Integer>) perm2.get(x).clone();
 
                     temp.addAll(perm2C);
 
-                    ArrayList<Integer> newPerm = temp;
+                    newPerm = temp;
 
                     perm.add(newPerm);
 
